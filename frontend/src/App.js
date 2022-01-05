d13088a862c9fda830a5776383caabb92fa9eac2
Saahil Sood <saahils365@gmail.com>
2022-01-05 18:01:51 -0500

    Modularized components and fixed styling issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import data from './data';
+import Product from './components/Product';
 
 function App() {
   return (
     <div className="grid-container">
     <header className="row">
         <div>
-            <a className="brand" href="/">amazona</a>
+            <a className="brand" href="/">DealDaddy</a>
         </div>
         <div>
             <a href="/">Cart</a>
@@ -17,36 +18,7 @@ function App() {
         <div className="row center">
           {
             data.products.map(product => (
-              <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                  <img className="medium" src={product.image} alt={product.name} />
-              </a>
-              <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                      <span>
-                          <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                          <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                          <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                          <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                          <i className="fa fa-star"></i>
-                      </span>
-                  </div>
-                  <div className="price">
-                      ${product.price}
-                  </div>
-              </div>
-            </div>
+             <Product product={product} />
             ))
           }
            

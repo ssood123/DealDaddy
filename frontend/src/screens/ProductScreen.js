f675e1340c39de9040d447dd82e9407c5de28051
Saahil Sood <saahils365@gmail.com>
2022-01-10 21:46:45 -0500

    Added redux to product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Product from '../components/Product';
 import Rating from '../components/Rating';
-import data from '../data';
+//import data from '../data';
+import { useDispatch, useSelector } from 'react-redux';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { detailsProduct } from '../actions/productActions';
 
 
 export default function ProductScreen(props) {
-    const product = data.products.find(x => x._id === props.match.params.id);
-    if (!product) {
-        return <div>Product Not Found</div>;
-    }
-    return (<div>
+    //const product = data.products.find(x => x._id === props.match.params.id);
+    const productDetails = useSelector((state) => state.productDetails)
+    const {loading, error, product} = productDetails;
+    const dispatch = useDispatch();
+    const productId = props.match.params.id;
+
+    useEffect(() => {
+        dispatch(detailsProduct(productId))
+    },[dispatch, productId])
+
+    return (
+
+        <div>
+        {loading ? (<LoadingBox></LoadingBox>) : 
+        error? (<MessageBox variant="danger">{error}</MessageBox>) :
+
+
+
+        <div>
         <Link to="/">Back to result</Link>
         <div className="row top">
             <div className="col-2">
@@ -63,5 +81,12 @@ export default function ProductScreen(props) {
             </div>
         </div>
     </div>
+
+
+
+        }
+      </div>
+    
+   
     )
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,13 @@
 import data from "./data";
 import {applyMiddleware, createStore, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 const initialState = {};
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer
 })
 
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));

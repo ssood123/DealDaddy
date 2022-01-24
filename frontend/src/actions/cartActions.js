7419786cc5c736871deac9e9c3b24a0919827167
Saahil Sood <saahils365@gmail.com>
2022-01-23 23:04:04 -0500

    Implemented a payment screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
 
 export const addToCart = (productId, qty) => async(dispatch, getState) => {
     const {data} = await axios.get(`/api/products/${productId}`);
@@ -26,4 +26,9 @@ export const removeFromCart = (productId) => (dispatch, getState) => {
 export const saveShippingAddress = (data) => (dispatch) => {
     dispatch({type: CART_SAVE_SHIPPING_ADDRESS, payload: data});
     localStorage.setItem('shippingAddress',JSON.stringify(data));
+}
+
+export const savePaymentMethod = (data) => (dispatch) => {
+    dispatch({type: CART_SAVE_PAYMENT_METHOD, payload: data});
+
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,7 @@ const initialState = {
     cart:{
         cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')):[],
         shippingAddress: localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')):{},
+        paymentMethod: 'PayPal',
     }
 };
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
